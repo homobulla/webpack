5f4563b39b3b9deae86c65b62a069df5fc622168
homobulla <homobulla@outlook.com>
2018-05-17 17:38:34 +0800

    server hot

--- app/js/greet.js
+++ app/js/greet.js
@@ -1,6 +1,6 @@
 module.exports = function() {
     var greet = document.createElement('div');
-    greet.textContent = "Hi there and greetings!";
+    greet.textContent = "deserver hot test server   " +  new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds();
     return greet;
   };
   
